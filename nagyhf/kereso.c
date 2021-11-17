feb6cc6ccfe5406909461e6b1868c9ec8b8ca0f5
barlord <64486378+barlord@users.noreply.github.com>
2021-11-17 19:19:29 +0100

    valami hogy menjen végre fel

---
+++ nagyhf/kereso.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+typedef struct recept {
+	char name[20];
+	char ossze[10];
+	char elkeszit[200];
+}Recept;
+
+//vesz ket chart és ha ugyanazok 1 et ad vissza különben 0
+int recept_search(const char* keres, const char* mibenkeres)
+{
+
+	if(strcmp(keres,mibenkeres)==0)
+	{
+		return 1;
+	}
+	return 0;
+
+}
+
+int main()
+{	
+	char keres[10];
+
+	//létrehoz egy receptet
+	Recept rantotta;
+
+	strcpy_s(rantotta.name,sizeof(rantotta.name), "rantotta");
+	strcpy_s(rantotta.ossze, sizeof(rantotta.ossze), "tojas");
+	strcpy_s(rantotta.elkeszit, sizeof(rantotta.elkeszit), "keverd ossze a tojast majd süssd ki");
+	
+	//bekéri a receptet
+	printf("ami van otthon:\n");
+	scanf_s("%s", &keres,10);
+	if (recept_search(keres, rantotta.ossze))
+	{
+		printf("\n ehez van minden otthon:");
+		printf("\n%s", rantotta.name);
+		printf("\n%s", rantotta.ossze);
+		printf("\n%s", rantotta.elkeszit);
+	}
+	else
+	{
+		printf("bad luck ehes maradsz");
+	}
+
+		return 0;
+}
